19c656a4f8e2d09d4e22786bc8bd98a791c1a11b
Kiran Mahn CS2021 <kiranmahn@gmail.com>
2024-09-30 17:06:33 +0100

    1.4 lab

--- cs407/1-4/main.java
+++ cs407/1-4/main.java
@@ -3,12 +3,34 @@
 // This lab uses JCA to make a simple cryptographer 
 // JCA stands for Java Cryptography Architecture
 
-import java.security.KeyPair;
+import java.security.*;
 
 public class main {
 
   public static void main(String[] args) {
-    System.out.println("Meow!");
+
+    // Create an instance of the MessageDigest class
+    MessageDigest md;
+
+    try {
+      md = MessageDigest.getInstance("SHA-256");
+
+      // Create an array of message bytes for input
+      String input = "Hello, World!";
+
+      // use the update method of MessageDigest to process the data
+      md.update(input.getBytes());
+
+      // Calculate the hash value
+      byte[] hashBytes = md.digest();
+
+      System.out.println("hash value: " + hashBytes);
+
+    } catch (NoSuchAlgorithmException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+
 
   }
 }
